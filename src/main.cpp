9570e9fe76be4735d6ba71d41a7afb93c7feda43
kajabrych <georgewbush6.6.1947@gmail.com>
2025-10-15 11:51:28 +0200

    my own branch

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main() {
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
---
+++ src/shader/shader.cpp
@@ -0,0 +1,11 @@
+#include "shader.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+shader::shader(const char* vertexPath,const char* fragmentPath){
+	//dostani shaderu z files
+	std::string vertexCode;
+	std::string fragmentCode;
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
+}
