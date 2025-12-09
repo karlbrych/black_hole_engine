9f03f6174eb0a4629b9b5efd2a9dfc31090c7767
karlbrych <georgewbush6.6.1947@gmail.com>
2025-12-09 21:09:57 +0100

    funkcni gitignore hopefully

--- build/CMakeFiles/Blackhole.dir/DependInfo.cmake
+++ build/CMakeFiles/Blackhole.dir/DependInfo.cmake
@@ -8,13 +8,13 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/almo/School/PRJ/black_hole_engine/src/glad.c" "CMakeFiles/Blackhole.dir/src/glad.c.o" "gcc" "CMakeFiles/Blackhole.dir/src/glad.c.o.d"
-  "/home/almo/School/PRJ/black_hole_engine/src/main.cpp" "CMakeFiles/Blackhole.dir/src/main.cpp.o" "gcc" "CMakeFiles/Blackhole.dir/src/main.cpp.o.d"
-  "/home/almo/School/PRJ/black_hole_engine/src/models/particle.cpp" "CMakeFiles/Blackhole.dir/src/models/particle.cpp.o" "gcc" "CMakeFiles/Blackhole.dir/src/models/particle.cpp.o.d"
-  "/home/almo/School/PRJ/black_hole_engine/src/models/shader.cpp" "CMakeFiles/Blackhole.dir/src/models/shader.cpp.o" "gcc" "CMakeFiles/Blackhole.dir/src/models/shader.cpp.o.d"
-  "/home/almo/School/PRJ/black_hole_engine/src/models/sphere.cpp" "CMakeFiles/Blackhole.dir/src/models/sphere.cpp.o" "gcc" "CMakeFiles/Blackhole.dir/src/models/sphere.cpp.o.d"
-  "/home/almo/School/PRJ/black_hole_engine/src/models/stb_image.cpp" "CMakeFiles/Blackhole.dir/src/models/stb_image.cpp.o" "gcc" "CMakeFiles/Blackhole.dir/src/models/stb_image.cpp.o.d"
-  "/home/almo/School/PRJ/black_hole_engine/src/models/texture.cpp" "CMakeFiles/Blackhole.dir/src/models/texture.cpp.o" "gcc" "CMakeFiles/Blackhole.dir/src/models/texture.cpp.o.d"
+  "/home/kajabrych/PRG/black_hole_engine/src/glad.c" "CMakeFiles/Blackhole.dir/src/glad.c.o" "gcc" "CMakeFiles/Blackhole.dir/src/glad.c.o.d"
+  "/home/kajabrych/PRG/black_hole_engine/src/main.cpp" "CMakeFiles/Blackhole.dir/src/main.cpp.o" "gcc" "CMakeFiles/Blackhole.dir/src/main.cpp.o.d"
+  "/home/kajabrych/PRG/black_hole_engine/src/models/particle.cpp" "CMakeFiles/Blackhole.dir/src/models/particle.cpp.o" "gcc" "CMakeFiles/Blackhole.dir/src/models/particle.cpp.o.d"
+  "/home/kajabrych/PRG/black_hole_engine/src/models/shader.cpp" "CMakeFiles/Blackhole.dir/src/models/shader.cpp.o" "gcc" "CMakeFiles/Blackhole.dir/src/models/shader.cpp.o.d"
+  "/home/kajabrych/PRG/black_hole_engine/src/models/sphere.cpp" "CMakeFiles/Blackhole.dir/src/models/sphere.cpp.o" "gcc" "CMakeFiles/Blackhole.dir/src/models/sphere.cpp.o.d"
+  "/home/kajabrych/PRG/black_hole_engine/src/models/stb_image.cpp" "CMakeFiles/Blackhole.dir/src/models/stb_image.cpp.o" "gcc" "CMakeFiles/Blackhole.dir/src/models/stb_image.cpp.o.d"
+  "/home/kajabrych/PRG/black_hole_engine/src/models/texture.cpp" "CMakeFiles/Blackhole.dir/src/models/texture.cpp.o" "gcc" "CMakeFiles/Blackhole.dir/src/models/texture.cpp.o.d"
   "" "Blackhole" "gcc" "CMakeFiles/Blackhole.dir/link.d"
   )
 
