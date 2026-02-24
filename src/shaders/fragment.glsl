e78b0cd7dbecb812e737575df13a53c8501c7398
marannnn <martin.hrubes1@proton.me>
2026-02-24 13:15:22 +0100

    added new line hopefully NOT on local account

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,8 +1,11 @@
 #version 330 core
 in vec2 TexCoords;
+
 out vec4 FragColor;
+
 uniform sampler2D diffuseTexture;
 uniform vec3 lightColor;
+
 void main() {
 	FragColor = texture(diffuseTexture, TexCoords) * vec4(1.0);
 }
